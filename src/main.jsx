686b8c8c63b8e5860db6c19f1e4566e5aa554dd9
vitoraraujocode <vitor3araujo@gmail.com>
2025-06-12 11:20:25 -0300

    Fix case sensitivity in TaskPage import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import TaskPage from "./pages/taskPage.jsx";
+import TaskPage from "./pages/TaskPage.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
