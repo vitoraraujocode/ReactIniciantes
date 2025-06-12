63cb9c13bb293ea642fdeaac3090489925f276ac
vitoraraujocode <vitor3araujo@gmail.com>
2025-06-12 11:31:00 -0300

    Fix Input component import and case sensitivity

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "./input";
+import Input from "./Input.jsx";
 
 function AddTasks({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
